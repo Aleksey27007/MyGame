de692c97fda78b333ade7dc0cd5ac17831e92b6f
Aleksey27007 <alekseyvaruj@gmail.com>
2025-03-15 16:49:57 +0300

    added sqlexception to method signature

--- src/main/java/com/my_game/service/author/AuthorService.java
+++ src/main/java/com/my_game/service/author/AuthorService.java
@@ -3,8 +3,8 @@ package com.my_game.service.author;
 import com.my_game.dto.AuthorDTO;
 import com.my_game.dto.BookDTO;
 import com.my_game.model.Author;
-import com.my_game.model.Book;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface AuthorService {
@@ -13,5 +13,5 @@ public interface AuthorService {
     Author updateNameAuthor(Long bookID, String newName);
     Author updateAuthorBook(Long bookID, AuthorDTO author);
     boolean deleteBook(Long bookID);
-    List<Author> getAuthors();
+    List<Author> getAuthors() throws SQLException;
 }
