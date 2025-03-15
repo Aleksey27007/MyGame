1557c2344c4233eb364bd1294efd23d8b6b67712
Aleksey27007 <alekseyvaruj@gmail.com>
2025-03-15 22:37:07 +0300

    fixed

--- src/main/java/com/my_game/dto/AuthorDTO.java
+++ src/main/java/com/my_game/dto/AuthorDTO.java
@@ -1,9 +1,13 @@
 package com.my_game.dto;
 
+import com.my_game.model.Book;
+
+import java.util.List;
+
 public class AuthorDTO {
 
     private String name;
     private String genre;
     private int age;
-
+    private List<Book> books;
 }
