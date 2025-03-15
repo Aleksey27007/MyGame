eebf480bd37be95134612dcc6889556b2e26e31f
Aleksey27007 <alekseyvaruj@gmail.com>
2025-03-15 23:17:18 +0300

    fixed book dto

--- src/main/java/com/my_game/dto/BookDTO.java
+++ src/main/java/com/my_game/dto/BookDTO.java
@@ -2,7 +2,11 @@ package com.my_game.dto;
 
 import com.my_game.model.Author;
 
-public class BookDTO {
-    private String name;
-    private Author author;
+public record BookDTO (
+        Integer id,
+        String name,
+        Author author
+){
+
 }
+
