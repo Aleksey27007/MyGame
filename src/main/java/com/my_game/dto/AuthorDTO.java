7d843c3d516b996f78592c2ecbf71b1ce1b20b3c
Aleksey27007 <alekseyvaruj@gmail.com>
2025-03-15 16:50:29 +0300

    added few row for DTO

--- src/main/java/com/my_game/dto/AuthorDTO.java
+++ src/main/java/com/my_game/dto/AuthorDTO.java
@@ -1,4 +1,9 @@
 package com.my_game.dto;
 
 public class AuthorDTO {
+
+    private String name;
+    private String genre;
+    private int age;
+
 }
--- src/main/java/com/my_game/dto/BookDTO.java
+++ src/main/java/com/my_game/dto/BookDTO.java
@@ -1,4 +1,8 @@
 package com.my_game.dto;
 
+import com.my_game.model.Author;
+
 public class BookDTO {
+    private String name;
+    private Author author;
 }
