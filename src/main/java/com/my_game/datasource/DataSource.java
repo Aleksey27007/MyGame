256e2f04c1f379f987c4d8472de9b3f9470f6ce5
Aleksey27007 <alekseyvaruj@gmail.com>
2025-03-15 13:46:24 +0300

    added DataSource properties

--- src/main/java/com/my_game/datasource/DataSource.java
+++ src/main/java/com/my_game/datasource/DataSource.java
@@ -1,4 +1,34 @@
 package com.my_game.datasource;
 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DataSource {
+    private static DataSource dataSource;
+    private final Properties properties = new Properties();
+//    private final ComboPooledDataSource pooledDataSource;
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new DataSource();
+        }
+        return dataSource;
+    }
+
+    private DataSource() {
+        try (InputStream input = new FileInputStream("src/main/resources/db.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
 }
